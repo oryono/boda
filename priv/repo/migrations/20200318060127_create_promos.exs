be24379a2f15b2e1d4747a4cb9486f3c4f60c590
Patrick Oryono <patricken08@gmail.com>
2020-03-19 18:02:20 +0300

    Removes events logic

--- priv/repo/migrations/20200318060127_create_promos.exs
+++ priv/repo/migrations/20200318060127_create_promos.exs
@@ -5,7 +5,6 @@ defmodule Boda.Repo.Migrations.CreatePromos do
     create table(:promos) do
       add :code, :string, null: false
       add :amount, :decimal, precision: 10, scale: 2
-#      add :event_id, references(:events, on_delete: :nothing)
       add :expiry_date, :date
       add :is_expired, :boolean, default: false, null: false
       add :is_active, :boolean, default: true, null: false
